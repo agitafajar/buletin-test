2951b8ccf0f9baed18cc641596e0fcb2ddd28d6d
agityoutap <agita.prabowo@ext.youtap.id>
2025-05-22 19:32:20 +0700

    feat(addfitur): add fitur

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="h-[100dvh] flex items-center justify-center bg-blue-500 p-4">
-      <h1 className="text-3xl font-bold underline text-white bg-red-500 p-4 text-center rounded w-full">
-        Hello world!
-      </h1>
-    </div>
-  );
-}
